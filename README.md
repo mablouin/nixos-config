4aea91f94c48db69ab54db78fac2681844ed2df6
mablouin <78802880+mablouin@users.noreply.github.com>
2025-05-28 19:46:22 -0400

    Basic flake setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# NixOS Configuration
+
+Bootsrap the NixOS configuration by running this command:
+
+```bash
+nix run --experimental-features 'nix-command flakes' github:mablouin/nixos-config
+```
---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "My NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.11";
+  };
+
+  outputs = inputs@{ self, nixpkgs, ... }:
+    nixosConfigurations.nixos = nixpkgs. lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+    };
+}

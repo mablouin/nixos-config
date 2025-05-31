58b6d223f5ce67030bdb0207b05a5cf283267a47
mablouin <78802880+mablouin@users.noreply.github.com>
2025-05-31 08:55:51 -0400

    add packages and apps

--- flake.nix
+++ flake.nix
@@ -8,11 +8,24 @@
   outputs = { self, nixpkgs, ... }@inputs:
     let
       lib = nixpkgs.lib;
+      system = "x86_64-linux";
     in {
       nixosConfigurations = {
         nixos = lib.nixosSystem {
-          system = "x86_64-linux";
+          system = system;
           modules = [ ./configuration.nix ];
+        };
+      };
+
+      packages.${system}.default = pkgs.writeScriptBin "install" ''
+        echo "flake installed"
+      '';
+
+      apps.${system} = {
+        default = self.apps.${system}.install;
+        install = {
+          type = "app";
+          program = "${self.packages.${system}.install}/bin/install";
         };
       };
     };

00ef84a714627c766ee8c56d7b812e242fcd24c8
mablouin <78802880+mablouin@users.noreply.github.com>
2025-06-04 08:01:51 -0400

    semi colon

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    }
+    };
   };
 
   outputs = { self, nixpkgs, nixos-wsl, home-manager, ... }@inputs:

99758bc73ca086a37acfe6768017902218e356e4
Marc-André Blouin <mablouin@genetec.com>
2026-03-10 14:51:21 +0000

    install azure credential provider

--- home/dotnet.nix
+++ home/dotnet.nix
@@ -3,6 +3,7 @@
 {
   home.packages = [
     pkgs.dotnet-sdk_10
+    pkgs.azure-artifacts-credprovider
   ];
 
   home.sessionPath = [
@@ -11,5 +12,6 @@
 
   home.sessionVariables = {
     DOTNET_ROOT = "${pkgs.dotnet-sdk_10}/share/dotnet";
+    NUGET_PLUGIN_PATHS = "${pkgs.azure-artifacts-credprovider}/lib/azure-artifacts-credprovider/CredentialProvider.Microsoft.dll";
   };
 }

ee6e3906a1a7790ba22a6863749aab4f1a33f158
Marc-André Blouin <mablouin@genetec.com>
2026-09-01 13:36:18 +0000

    Remove dotnet pinning

--- home/dotnet.nix
+++ home/dotnet.nix
@@ -1,9 +1,9 @@
-{ pkgs-dotnet, ... }:
+{ pkgs-unstable, ... }:
 
 {
   home.packages = [
-    pkgs-dotnet.dotnet-sdk_10
-    pkgs-dotnet.azure-artifacts-credprovider
+    pkgs-unstable.dotnet-sdk_10
+    pkgs-unstable.azure-artifacts-credprovider
   ];
 
   home.sessionPath = [
@@ -11,7 +11,7 @@
   ];
 
   home.sessionVariables = {
-    DOTNET_ROOT = "${pkgs-dotnet.dotnet-sdk_10}/share/dotnet";
-    NUGET_PLUGIN_PATHS = "${pkgs-dotnet.azure-artifacts-credprovider}/lib/azure-artifacts-credprovider/CredentialProvider.Microsoft.dll";
+    DOTNET_ROOT = "${pkgs-unstable.dotnet-sdk_10}/share/dotnet";
+    NUGET_PLUGIN_PATHS = "${pkgs-unstable.azure-artifacts-credprovider}/lib/azure-artifacts-credprovider/CredentialProvider.Microsoft.dll";
   };
 }
